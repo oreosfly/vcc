1358809e82bc9b4a5954dda1d0ba618561850a77
hurrytospring <ddyz1993@163.com>
2018-12-07 16:36:23 +0800

    del password

---
+++ server/services/sendmail.ts
@@ -0,0 +1,24 @@
+const email = require("emailjs");
+const server = email.server.connect({
+    user: "yuanzhang",
+    password: ".....sdafsd",
+    host: "smtp.exmail.qq.com",
+    ssl: true,//465,
+    port: 465
+});
+export default async function send(token: string, emailAddr: string) {
+    const msg = {
+        text: `本次登陆token${token} 有效期20min，请尽快登陆使用`,
+        from: "yuanzhang@vite.org",
+        to: `${emailAddr}`,
+    };
+    return new Promise((res, rej) => {
+        server.send(msg, function (err: Error, msg: string) {
+            if (err) {
+                rej(err)
+            } else {
+                res(msg)
+            }
+        })
+    })
+}
